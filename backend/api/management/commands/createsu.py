ed83a0206d43deaec27a7f8d40ce9de16fc1fd6d
adityacp <aditya94palaparthy@gmail.com>
2022-05-15 01:21:00 +0530

    Update management command

--- backend/api/management/commands/createsu.py
+++ backend/api/management/commands/createsu.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import User
 from django.core.management.base import BaseCommand
 
+from api.models import Account
+
 
 class Command(BaseCommand):
     help = 'Creates a superuser.'
@@ -12,3 +14,10 @@ class Command(BaseCommand):
                 password='Python321'
             )
         print('Superuser has been created.')
+        try:
+            for i in range(1, 11):
+                entry = "test_user"+str(i)
+                u = User.objects.create_user(username=entry, password=entry, first_name=entry, last_name=entry)
+                Account.objects.create(user=u, balance=10000)
+        except Exception:
+            pass
