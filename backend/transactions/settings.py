c7d4e1250473d1a51a3522e0b89ca8a4327aa67b
adityacp <aditya94palaparthy@gmail.com>
2022-05-15 00:37:54 +0530

    Update settings

--- backend/transactions/settings.py
+++ backend/transactions/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config("DEBUG") == "True"
+DEBUG = config("DEBUG", "True") == "True"
 
 ALLOWED_HOSTS = ["*"]
 
